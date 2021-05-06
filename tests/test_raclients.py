568f9dce0e0bfcc6ae95e01adff9da3f94528443
Emil Madsen <emil@magenta.dk>
2021-05-06 13:04:53 +0200

    Fixup test-suite

--- tests/test_raclients.py
+++ tests/test_raclients.py
@@ -3,7 +3,7 @@
 # SPDX-FileCopyrightText: 2021 Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
 # --------------------------------------------------------------------------------------
-from ra_clients import __version__
+from raclients import __version__
 
 
 def test_version():
